f688cf517bd90c001954a0c28d092480ac95ab17
Ahmad Diab <aadiab09@gmail.com>
2020-04-17 00:58:35 -0500

    Model2
    Fixed the training of model, uploaded the correct one _2

--- word2vec.py
+++ word2vec.py
@@ -7,8 +7,10 @@ q_file = open(mscoco_q_path, 'r')
 sentences = []
 lines = q_file.readlines()
 for line in lines:
-    sentences.append([line.strip()])
+    #print(line.strip())
+    sentences.append(line.strip().split(' '))
 q_file.close()
+#print(sentences)
 print("VQA Answers .. building word2vec model")
 mscoco_a_path = "datasets\\qa\\cocoqa\\train\\answers.txt"
 a_file = open(mscoco_a_path, 'r')
@@ -20,6 +22,6 @@ a_file.close()
 print("Training model")
 model = Word2Vec(sentences, min_count=1)
 print("Saving model ")
-model.save("w2v_mscocovqa.model")
+model.save("w2v_mscocovqa_2.model")
 print("Model is done, saved in \'w2v_mscocovqa.model\' file")
 
